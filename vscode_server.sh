f962b3de72791fe3f9d669a1458de2bc099a1090
Kislitsin Pavel <48181286+KPN10@users.noreply.github.com>
2023-10-18 18:52:12 +0700

    Add files via upload

---
+++ vscode_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+commit_id=f06011ac164ae4dc8e753a3fe7f9549844d15e35
+
+# Download url is: https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable
+curl -sSL "https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable" -o vscode-server-linux-x64.tar.gz
+
+mkdir -p ~/.vscode-server/bin/${commit_id}
+tar zxvf vscode-server-linux-x64.tar.gz -C ~/.vscode-server/bin/${commit_id} --strip 1
+touch ~/.vscode-server/bin/${commit_id}/0
