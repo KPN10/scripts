b9d435162d66d3725dd1c6e40864956f76460bdb
Kislitsin Pavel <48181286+KPN10@users.noreply.github.com>
2024-02-26 13:29:09 +0700

    Update setup_terminal.sh

--- setup_workstation/setup_terminal.sh
+++ setup_workstation/setup_terminal.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Short path
 sed -i '/sPS1=\'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \'/\
 	PS1=\'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ \''\
 	~/.bashrc
 
+# Hide password
+echo 'Defaults !pwfeedback' | sudo tee /etc/sudoers.d/9_no_pwfeedback
