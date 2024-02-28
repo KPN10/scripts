39d4535c92502b251031ddf16883f9bd6a763aab
Kislitsin Pavel <kislitsin.pn@yandex.ru>
2024-02-28 12:25:30 +0700

    Update configure_git.sh

--- setup_workstation/configure_git.sh
+++ setup_workstation/configure_git.sh
@@ -4,7 +4,8 @@ user_name=""
 user_email=""
 editor=vim
 
-git config --global user.name $user_name
-git config --global user.email $user_email
+git config --global user.name "${user_name}"
+git config --global user.email "${user_email}"
 git config --global core.editor $editor
 git config --global init.defaultBranch main
+
