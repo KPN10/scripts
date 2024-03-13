cdda7db838e3241ea76fc36747aed538b46d23d4
Kislitsin Pavel <kislitsin.pn@yandex.ru>
2024-03-13 20:39:04 +0700

    Updated docker_proxy.sh

--- docker_proxy.sh
+++ docker_proxy.sh
@@ -2,18 +2,18 @@
 
 sudo mkdir -p /etc/systemd/system/docker.service.d
 
-myproxy_hostname="ip"
-port="port"
+ip="$1"
+port="$2"
 
 content="\
 [Service]\n\
-Environment=\"HTTP_PROXY=http://${myproxy_hostname}:${port}\"\n\
-Environment=\"HTTPS_PROXY=http://${myproxy_hostname}:${port}\"\n\
+Environment=\"HTTP_PROXY=http://${ip}:${port}\"\n\
+Environment=\"HTTPS_PROXY=http://${ip}:${port}\"\n\
 Environment=\"NO_PROXY=\"localhost,127.0.0.1,::1\"\n\
 "
 
-sudo echo $content > /etc/systemd/system/docker.service.d/proxy.conf
-echo -e $content > proxy.conf
+sudo echo -e $content > /etc/systemd/system/docker.service.d/proxy.conf
+# echo -e $con::tent > proxy.conf
 
 sudo systemctl daemon-reload
 sudo systemctl restart docker.service
