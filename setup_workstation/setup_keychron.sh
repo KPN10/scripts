44f8293d64d46f5ecdd16862b017ecc4c5bb7e0e
Kislitsin Pavel <kislitsin.pn@yandex.ru>
2025-01-09 10:19:50 +0700

    ~

--- setup_workstation/setup_keychron.sh
+++ setup_workstation/setup_keychron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
+echo "options hid_apple fnmode=1" | sudo tee /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u -k all
-sudo systemctl reboot
+# sudo systemctl reboot
 
